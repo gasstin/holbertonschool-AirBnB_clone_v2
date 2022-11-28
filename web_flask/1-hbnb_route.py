d7455018145e4d2206298af8797bc09a04c5e44d
gasstin <gastonl1994@gmail.com>
2022-11-28 10:35:37 -0300

    adding task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application:
+""" task 1
+
+    Write a script that starts a Flask web application:
     Your web application must be listening on 0.0.0.0, port 5000
     Routes:
     /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
     You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask, request
@@ -16,6 +19,11 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def task_0():
     return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def task_1():
+    return "HBNB"
 
 
 if __name__ == '__main__':
